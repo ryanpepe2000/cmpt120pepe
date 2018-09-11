b86288e2f12ba5d2d5ef3327d5ce984e5e751a3b
Ryan Pepe <ryan.pepe1@marist.edu>
2018-09-11 17:22:54 -0400

    Solution to Simple Loop exercise.

---
+++ evens.py
@@ -0,0 +1,10 @@
+# evens.py
+# Program that prints a list of all even numbers in the range 1-20
+
+def main():
+    print("All even numbers from 1-20 are:")
+    print()
+    
+    for i in range(2,21,2):
+        print(i)
+main()
