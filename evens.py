da2202b04bedbe80292513d7666b00e6e47a0dd5
Ryan Pepe <ryan.pepe1@marist.edu>
2018-09-11 17:55:47 -0400

    Solution to listing even numbers from 1-20

--- evens.py
+++ evens.py
@@ -2,6 +2,9 @@
 # Program that prints a list of all even numbers in the range 1-20
 
 def main():
-    for i in range(20):
+    print("All even numbers from 1-20 are:")
+    print()
+    
+    for i in range(2,21,2):
         print(i)
 main()
