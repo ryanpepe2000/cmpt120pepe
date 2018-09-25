e896e4daa475fcb93340fa9ada8ef371bf7c20a3
Ryan Pepe <ryan.pepe1@marist.edu>
2018-09-25 17:43:40 -0400

    Added '.' in between first and last name, and required 8 character password.

--- credentials.py
+++ credentials.py
@@ -3,24 +3,27 @@
 # Author: Ryan Pepe
 # Created: 2018-09-25
 
+
+
 def main():
 
     # get user's first and last names
     first = input("Enter your first name: ")
     last = input("Enter your last name: ")
-
+    
     # TODO modify this to generate a Marist-style username
-    uname = first + last
+    uname = first + "." + last
 
     # ask user to create a new password
     passwd = input("Create a new password: ")
 
     # TODO modify this to ensure the password has at least 8 characters
-    while False:
+    while len(passwd) < 8:
         print("Fool of a Took! That password is feeble!")
         passwd = input("Create a new password: ")
     print("The force is strong in this one…")
     print("Account configured. Your new email address is",
         uname + "@marist.edu")
+
 
 main()
