2235aa7f7ee2375052cc18c6bc98a6454f3333ba
Ryan Pepe <ryan.pepe1@marist.edu>
2018-09-25 17:17:36 -0400

    Added program that creates Marist style username and password.

---
+++ credentials.py
@@ -0,0 +1,26 @@
+# CMPT 120 Intro to Programming
+# Lab #5 – Working with Strings and Functions
+# Author: Ryan Pepe
+# Created: 2018-09-25
+
+def main():
+
+    # get user's first and last names
+    first = input("Enter your first name: ")
+    last = input("Enter your last name: ")
+
+    # TODO modify this to generate a Marist-style username
+    uname = first + last
+
+    # ask user to create a new password
+    passwd = input("Create a new password: ")
+
+    # TODO modify this to ensure the password has at least 8 characters
+    while False:
+        print("Fool of a Took! That password is feeble!")
+        passwd = input("Create a new password: ")
+    print("The force is strong in this one…")
+    print("Account configured. Your new email address is",
+        uname + "@marist.edu")
+
+main()
