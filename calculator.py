0b69df272ac3a7416539dac63c3a6177a379276a
Ryan Pepe <ryan.pepe1@marist.edu>
2018-09-28 03:27:24 -0400

    Created variable to store user input as a list.

--- calculator.py
+++ calculator.py
@@ -5,3 +5,11 @@
 
 def main():
     equation = str(input("Enter an equation with any basic arithmetic operator: "))
+
+    # Turns user's input into a list of each number and arithmetic operator, regardless
+    # of whether the user inputted spaces in between operators.
+    equationList = equation.replace("+", " + ").replace("-"," - ").replace("*"," * ").replace("/"," / ").split()
+
+    
+    
+main()
