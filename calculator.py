52bcc5e7e9dd6b30b6e8a7ecc86ebba2572f3c9a
Ryan Pepe <ryan.pepe1@marist.edu>
2018-10-01 02:03:36 -0400

    Accounted for negative numbers in initial list creation.

--- calculator.py
+++ calculator.py
@@ -3,12 +3,14 @@
 # Date Created: 2018-09-28
 # Author: Ryan Pepe
 
+from operator import *
+
 def main():
     equation = str(input("Enter an equation with any basic arithmetic operator: "))
 
     # Turns user's input into a list of each number and arithmetic operator, regardless
     # of whether the user inputted spaces in between operators.
-    equationList = equation.replace("+", " + ").replace("-"," - ").replace("*"," * ").replace("/"," / ").split()
+    equationList = equation.replace("+", " + ").replace("- "," - ").replace("*"," * ").replace("/"," / ").split()
 
     # Creates blank lists that for each operation and the solution.
     first = []
@@ -19,6 +21,7 @@ def main():
     order(equationList, first, second)
     print(first)
     print(second)
+    print()
 
 ## Function that marks the location of each arithmetic operator in seperate lists
 def order(equation, first, second):
@@ -31,6 +34,8 @@ def order(equation, first, second):
 
 
 
+            
+        
 
 
 
