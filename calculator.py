7006898559734577ea66f8294629ceb52f2edb50
Ryan Pepe <ryan.pepe1@marist.edu>
2018-10-01 17:40:57 -0400

    Deleted unnecessary code and added comments. 1 Bug found with nefatives.

--- calculator.py
+++ calculator.py
@@ -3,44 +3,21 @@
 # Date Created: 2018-09-28
 # Author: Ryan Pepe
 
-from operator import *
-
 def main():
     equation = str(input("Enter an equation with any basic arithmetic operator: "))
 
     # Turns user's input into a list of each number and arithmetic operator, regardless
     # of whether the user inputted spaces in between operators.
     equationList = equation.replace("+", " + ").replace("*"," * ").replace("/"," / ").split()
-
-    # Creates blank lists that for each operation and the solution.
-    first= []
-    second = []
-
-
-    
-    solution = []
-    
-    order(equationList, first, second)
-
     print(equationList)
-    
+    # First computes multiplication/division, then computes addition/subtraction.
     multDiv(equationList)
     addSub(equationList)
-    
 
-
-## Function that marks the location of each arithmetic operator in seperate lists
-
-def order(equation, first, second):
-    for i, e in enumerate(equation):
-        if e == "*" or e == "/":
-            first.append(i)
-
-    for i, e in enumerate(equation):
-        if e == "+" or e == "-":
-            second.append(i)
-            
-
+    # Displays solution
+    print()
+    print("Solution: ", equationList[0])
+# Function that computes multiplication/division.          
 def multDiv(eq):
     while "*" in eq or "/" in eq:
         for i, e in enumerate(eq):
@@ -51,7 +28,7 @@ def multDiv(eq):
                 eq[i] = float(eq[i-1]) / float(eq[i+1])
                 del eq[i+1],eq[i-1]
     print(eq)
-            
+# Function that computes addition/subtraction.          
 def addSub(eq):
     while "+" in eq or "-" in eq:
         for i, e in enumerate(eq):
@@ -62,28 +39,6 @@ def addSub(eq):
                 eq[i] = float(eq[i-1]) - float(eq[i+1])
                 del eq[i+1],eq[i-1]
     print(eq)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 main()
 
 
