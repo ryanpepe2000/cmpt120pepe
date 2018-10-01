be52306ae9cedb547a851eced9c34f0e2838354d
Ryan Pepe <ryan.pepe1@marist.edu>
2018-10-01 17:14:43 -0400

    Created function to do addition/subraction second.

--- calculator.py
+++ calculator.py
@@ -10,7 +10,7 @@ def main():
 
     # Turns user's input into a list of each number and arithmetic operator, regardless
     # of whether the user inputted spaces in between operators.
-    equationList = equation.replace("+", " + ").replace("-"," - ").replace("*"," * ").replace("/"," / ").split()
+    equationList = equation.replace("+", " + ").replace("*"," * ").replace("/"," / ").split()
 
     # Creates blank lists that for each operation and the solution.
     first= []
@@ -24,14 +24,13 @@ def main():
 
     print(equationList)
     
-    solve(equationList, first, second, solution)
-    print(first)
-    print(second)
-    print()
-    print(solution)
+    multDiv(equationList)
+    addSub(equationList)
+    
 
 
 ## Function that marks the location of each arithmetic operator in seperate lists
+
 def order(equation, first, second):
     for i, e in enumerate(equation):
         if e == "*" or e == "/":
@@ -40,24 +39,9 @@ def order(equation, first, second):
     for i, e in enumerate(equation):
         if e == "+" or e == "-":
             second.append(i)
+            
 
-
-#def solve(eq, pos1, pos2, sol):
-#    for i, e in enumerate(pos1):
-#       if eq[e] == "*":
-#            sol.append(float(eq[e-1]) * float(eq[e+1]))
-#            
-#           eq[e-1] = "TEMP"
-#          eq[e] = "TEMP"
-#         eq[e+1] = "TEMP"
-#        elif eq[e] == "/":
-#            sol.append(float(eq[e-1]) / float(eq[e+1]))
-#            
-#            eq[e-1] ="TEMP"
-#            eq[e] = "TEMP"
-#            eq[e+1] = "TEMP"
-
-def solve(eq,first, second, sol):
+def multDiv(eq):
     while "*" in eq or "/" in eq:
         for i, e in enumerate(eq):
             if e == "*":
@@ -68,8 +52,16 @@ def solve(eq,first, second, sol):
                 del eq[i+1],eq[i-1]
     print(eq)
             
-        
-
+def addSub(eq):
+    while "+" in eq or "-" in eq:
+        for i, e in enumerate(eq):
+            if e == "+":
+                eq[i] = float(eq[i-1]) + float(eq[i+1])
+                del eq[i+1],eq[i-1]
+            elif e == "-":
+                eq[i] = float(eq[i-1]) - float(eq[i+1])
+                del eq[i+1],eq[i-1]
+    print(eq)
 
 
 
