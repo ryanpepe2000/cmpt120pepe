871c6255e9b5861c8d0588ecf880c28218b2b8a2
Ryan Pepe <ryan.pepe1@marist.edu>
2018-10-02 17:21:34 -0400

    Added break to for loops which search for operators.

--- calculator.py
+++ calculator.py
@@ -17,6 +17,7 @@ def main():
     # Displays solution
     print()
     print("Solution: ", equationList[0])
+
 # Function that computes multiplication/division.          
 def multDiv(eq):
     while "*" in eq or "/" in eq:
@@ -24,21 +25,43 @@ def multDiv(eq):
             if e == "*":
                 eq[i] = float(eq[i-1]) * float(eq[i+1])
                 del eq[i+1],eq[i-1]
+                break
             elif e == "/":
                 eq[i] = float(eq[i-1]) / float(eq[i+1])
                 del eq[i+1],eq[i-1]
+                break
     print(eq)
+    
 # Function that computes addition/subtraction.          
 def addSub(eq):
     while "+" in eq or "-" in eq:
         for i, e in enumerate(eq):
             if e == "+":
-                eq[i] = float(eq[i-1]) + float(eq[i+1])
+                eq[i] = float(eq[i+1]) + float(eq[i-1])
                 del eq[i+1],eq[i-1]
+                break
             elif e == "-":
                 eq[i] = float(eq[i-1]) - float(eq[i+1])
                 del eq[i+1],eq[i-1]
+                break
+               
     print(eq)
+    
 main()
 
+#### 8 / 2 + 4 * 4 - 3 + 2 * 2 / 6 DOES NOT WORK == 16.3333333333
+## TRULY EQUALS 17.6666666666666666666
 
+#Class PRoject notes
+# range(1, len(list), 2)
+# Looks at all operators
+#
+# while len((list) > 1
+
+#for i in range(1,len(list),2)
+# if list[i] == "*"
+#   result = float(list[i-1]) * float(list[i+1])
+#    del list[i-1:i+1]
+#   list.insert(i-1, result)
+#   break
+# elif list[i] == "/"
