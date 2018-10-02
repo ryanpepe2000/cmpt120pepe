88b3aea062d1ea9ba82299fbd7b4efdc77ec1776
Ryan Pepe <ryan.pepe1@marist.edu>
2018-10-02 18:06:36 -0400

    Resolved issues #3, #4, #5, #6. Forgot to do '--ammend'

--- guessing-game.py
+++ guessing-game.py
@@ -9,9 +9,14 @@ def main():
         print("Thinking of an animal...\n")
         name = str(input("Guess the type of animal: ")).lower()
         if name == "pig":
-            print("\nCongratulations, you guessed correctly!")
+            print("\nCongratulations, you guessed correctly!\n")
+            likeAnimal = str(input("Do you like this animal?: "))
+            if likeAnimal[0].lower() == "y":
+                print("\nThat's awesome, me too!")
+            elif likeAnimal[0].lower() == "n":
+                print("\nWhy not!? Pigs are the best!")
             break
-        elif name == "quit":
+        elif name[0].lower() == "q":
             break
         else:
             print("\nSorry, you guessed wrong. Try again!\n")
