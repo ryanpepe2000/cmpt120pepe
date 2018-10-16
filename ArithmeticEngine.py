e96c1dc312ec81f87cf8d330230f8d11d93871d1
Ryan Pepe <ryan.pepe1@marist.edu>
2018-10-16 18:07:35 -0400

    Deals with non-numeric inputs and divide by zero

--- ArithmeticEngine.py
+++ ArithmeticEngine.py
@@ -2,41 +2,62 @@
 # Ryan Pepe
 # 10/16/2018
 ###
+
 def showIntro():
     print("Welcome to the Arithmetic Engine!")
     print("=================================\n")
     print("Valid commands are 'add', 'mult', 'sub', 'div', and 'quit'.\n")
+
 def showOutro():
     print("\nThank you for using the Arithmetic Engine…")
     print("\nPlease come back again soon!")
+
 def doLoop():
     while True:
         cmd = input("What computation do you want to perform? ").lower()
         if cmd == "add" or cmd == "sub" or cmd == "mult" or cmd == "div":
+
             if cmd == "add":
-                num1 = int(input("Enter the first number: "))
-                num2 = int(input("Enter the second number: "))
+                num1, num2 = numbers()
                 result = num1 + num2
+
             elif cmd == "sub":
-                num1 = int(input("Enter the first number: "))
-                num2 = int(input("Enter the second number: "))
+                num1, num2 = numbers()
                 result = num1 - num2
+
             elif cmd == "mult":
-                num1 = int(input("Enter the first number: "))
-                num2 = int(input("Enter the second number: "))
+                num1, num2 = numbers()
                 result = num1 * num2
+
             elif cmd == "div":
-                num1 = int(input("Enter the first number: "))
-                num2 = int(input("Enter the second number: "))
-                result = num1 // num2
+                num1, num2 = numbers()
+                try:
+                    result = num1 // num2
+                except:
+                    print("Error: Divide by zero")
+                    continue
+                
             print("The result is " + str(result) + ".\n")
+            
         elif cmd == "quit":
             break
         else:
             print("'"+cmd+"'", "is not a valid command.")
 
+def numbers():
+    while True:
+        try:
+            num1 = int(input("Enter the first number: "))
+            num2 = int(input("Enter the second number: "))
+            return num1, num2
+        except:
+            print("Invalid number")
+
+                
 def main():
+
     showIntro()
     doLoop()
     showOutro()
+    
 main()
