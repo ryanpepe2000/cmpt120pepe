5d09707ccbb05cd7e4fd6ba4c038fb6d37ab32ac
Ryan Pepe <ryan.pepe1@marist.edu>
2018-10-26 03:27:43 -0400

    Created working keypad that calls to calculator.py

--- keypad.py
+++ keypad.py
@@ -2,8 +2,10 @@
 # This program builds a graphical calculator display.
 
 from graphics import *
+import calculator
+import sys
 
-
+# Creats rectangles with labels and returns those values.
 def createButton(values):
     p1 = Point(values[0], values[1])
     p2 = Point(values[0]+ 1, values[1] + 1)
@@ -12,6 +14,7 @@ def createButton(values):
     label = Text(Point(values[0] + 0.5, values[1] + 0.5), values[2])
     return button, label
 
+# Creates key on keypad for each item in list
 def createKeypad(lst):
     keys = []
     for key in lst:
@@ -19,6 +22,7 @@ def createKeypad(lst):
         keys.append([button, label])
     return keys
 
+# Creates a rectangle for screen and returns coordinates and screen text.
 def createScreen(screenText):
     
     p1 = Point(0,5)
@@ -28,6 +32,7 @@ def createScreen(screenText):
     screenText = Text(Point(2,5.5),screenText)
     return screen, screenText
 
+# Draws all objects
 def renderObjects(keys, win, screen, screenText):
     for key in keys:
         key[0].draw(win)
@@ -35,6 +40,7 @@ def renderObjects(keys, win, screen, screenText):
     screen.draw(win)
     screenText.draw(win)
 
+# Stores the string of label for each button pressed.
 def click(win, keys):
     mouse = win.getMouse()
     
@@ -44,7 +50,15 @@ def click(win, keys):
         if button.p1.x < mouse.x < button.p2.x and button.p1.y < mouse.y < button.p2.y:
             return(keys[e][2])
 
+# Creates a value for each button pressed in calculator.
+# Numbers/decimals are stored as one item in list.
+# Arithmetic operators are stored as one value
+# "Del" and "+/-" are stored as one.
+# "=" calculates function
+
 def createNumber(win,num,numList,keyList,mouse, screenText):
+
+    # NUMBERS
     if mouse != "*" and mouse != "/" and mouse != "+" and mouse != "-" and mouse != "=" and mouse != "+/-" and mouse != "Del":
         if mouse != ".":
             num.append(str(mouse))
@@ -52,7 +66,7 @@ def createNumber(win,num,numList,keyList,mouse, screenText):
             num.append(".")
         print(num)
        
-        
+    # ARITHMETIC OPERATORS  
     elif mouse == "*" or mouse == "/" or mouse == "+" or mouse == "-":
         try:
             if float(numList[-1]) > 0:
@@ -72,27 +86,32 @@ def createNumber(win,num,numList,keyList,mouse, screenText):
                 numList.append(mouse)
             print(numList)
             num.clear()
-        
+
+    # NEGATIVE BUTTON
     elif mouse == "+/-":
         numList.append("".join(num))
         numList[-1] = str(float(numList[-1]) * -1)
 
         print(numList)
-     
+
+    # DELETE BUTTON
     elif mouse == "Del":
         numList.clear() 
         num.clear()
         screenText.setText("")
-        
+
+    # CALCULATE BUTTON
     elif mouse == "=":
         try:
             if float(numList[-1]) > 0:
                 numList.append("".join(num))
                 print(numList)
+                print(solution)
         except:
             numList.append("".join(num))
             print(numList)
 
+        
 # Sets text in screen to the list of values.
 def display(win,num,numList,screenText):
     screenText.setText(" ".join(numList))
@@ -105,7 +124,16 @@ def loopCalc(win,screen,numList,keyList,screenText):
             mouse = click(win,keyList)
             createNumber(win,num,numList,keyList,mouse,screenText)
             display(win, num, numList,screenText)
-            print(numList)
+
+            # Calls to calculate.py to compute solution
+            if mouse == "=":
+                solution = calculator.main(numList)
+                screenText.setText(solution)
+
+            # QUIT BUTTON
+            if mouse == "Quit":
+                win.close()
+                pass
 
 def main():
     numberList = []
@@ -114,7 +142,7 @@ def main():
               [0,1,"1","navajoWhite"],   [1,1,"2","navajoWhite"], [2,1,"3","navajoWhite"],   [3,1,"+","orange"],
               [0,2,"4","navajoWhite"],   [1,2,"5","navajoWhite"], [2,2,"6","navajoWhite"],   [3,2,"-","orange"],
               [0,3,"7","navajoWhite"],   [1,3,"8","navajoWhite"], [2,3,"9","navajoWhite"],   [3,3,"*","orange"],
-              [0,4,"","white"],          [1,4,"","white"],        [2,4,"Del","orange"],      [3,4,"/","orange"]]
+              [0,4,"Quit","white"],          [1,4,"","white"],        [2,4,"Del","orange"],      [3,4,"/","orange"]]
 
     keys = createKeypad(keyList)
     screen, screenText = createScreen(numberList)
