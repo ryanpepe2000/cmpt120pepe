538f7f5a41c8559bc3672dda01f8e66ace901a6c
Ryan Pepe <ryan.pepe1@marist.edu>
2018-11-06 01:27:52 -0500

    Removed old "calculator.py" and replaced with "calculator_functions.py".

---
+++ calculator_functions.py
@@ -0,0 +1,47 @@
+# calculator.py
+# CMPT 120
+# Date Created: 2018-09-28
+# Author: Ryan Pepe
+
+
+def solve(expressionList):
+
+
+    # Turns user's input into a list of each number and arithmetic operator, regardless
+    # of whether the user inputted spaces in between operators.
+    #expressionList = expression.replace("+", " + ").replace("*"," * ").replace("/"," / ").split()
+
+    # First computes multiplication/division, then computes addition/subtraction by .
+    multDiv(expressionList)
+    addSub(expressionList)
+
+    # Displays solution
+    return (expressionList[0])
+
+# Function that computes multiplication/division by enumerating list ([i, e] = [array, string]).         
+def multDiv(ex):
+    while "*" in ex or "/" in ex:
+        for i, e in enumerate(ex):
+            if e == "*":
+                ex[i] = float(ex[i-1]) * float(ex[i+1])
+                del ex[i+1],ex[i-1]
+                break
+            elif e == "/":
+                ex[i] = float(ex[i-1]) / float(ex[i+1])
+                del ex[i+1],ex[i-1]
+                break
+
+# Function that computes addition/subtraction by enumerating list ([i, e] = [array, string]).          
+def addSub(ex):
+    while "+" in ex or "-" in ex:
+        for i, e in enumerate(ex):
+            if e == "+":
+                ex[i] = float(ex[i+1]) + float(ex[i-1])
+                del ex[i+1],ex[i-1]
+                break
+            elif e == "-":
+                ex[i] = float(ex[i-1]) - float(ex[i+1])
+                del ex[i+1],ex[i-1]
+                break
+    
+
